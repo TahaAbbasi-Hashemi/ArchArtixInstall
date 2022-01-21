e7b39ca831faba5654dc4328dfcaf8ca447d0e36
Taha Abbasi-Hashemi <taha.ahashemi@gmail.com>
2022-01-20 20:21:36 -0500

    fixing artix instll

--- api.sh
+++ api.sh
@@ -7,7 +7,6 @@ un=taha
 cn=lvmsys
 vn=sys
 hn=beryllium
-
 up=qw
 cp=qw
 rp=qw
@@ -37,7 +36,7 @@ sgdisk --mbrtogpt $dri
 sgdisk --new $pen::$pes --typecode $pen:ef00 --change-name $pen:"EFI" $dri
 sgdisk --new $pbn::$pbs --typecode $pbn:ef00 --change-name $pbn:"BOOT" $dri
 sgdisk --new $psn::$pss --typecode $psn:8304 --change-name $psn:"SYS" $dri
-partprobe $dri $cn
+partprobe $dri
 wipefs -af $pe
 wipefs -af $pb
 wipefs -af $ps
@@ -46,9 +45,8 @@ wipefs -af $ps
 #Encrypting
 modprobe dm_crypt
 modprobe dm_mod
-#cryptsetup -v --type luks2 -h sha512 luksFormat $ps
 cryptsetup -v --iter-time 5000 --type luks2 --hash sha512 luksFormat $ps
-crypttesup open $ps $
+crypttesup open $ps $cn
 pvcreate /dev/mapper/$cn
 vgcreate $vn /dev/mapper/$cn
 lvcreate -L 1G $vn -n etc
