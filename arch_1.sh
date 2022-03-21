78453c6a21bec763984fd8424fc1bf6488f955e6
Taha Abbasi-Hashemi <taha.ahashemi@gmail.com>
2022-03-21 18:40:25 -0400

    arch fixing

--- arch_1.sh
+++ arch_1.sh
@@ -17,7 +17,7 @@ modprobe dm_mod
 sgdisk --zap-all "$bootdrive"
 sgdisk --mbrtogpt "$bootdrive"
 sgdisk --new 1::+1G --typecode 1:ef00 --change-name 1:"ESP" "$bootdrive"
-sgdisk --new 2:::   --typecode 2:8300 --change-name 2:"SUB" "$bootdrive"
+sgdisk --new 2:::   --typecode 2:8300 --change-name 2:"USB" "$bootdrive"
 partprobe $bootdrive
 wipefs -af $bootdrive"1"
 wipefs -af $bootdrive"2"
@@ -46,70 +46,39 @@ gpg --decrypt /tmp/efiboot/key.gpg | cryptsetup --cipher serpent-xts-plain64 --k
 cryptsetup luksHeaderBackup $bootdriveP --header-backup-file /tmp/efiboot/header.img 
 
 
-#Making the BTRFS subsystem
+#Opening the drive for read and write plus setting as btrfs
 echo RELOADAGENT | gpg-connect-agent
 gpg --decrypt /tmp/efiboot/key.gpg | cryptsetup --key-file - luksOpen $bootdriveP root
 mkfs.btrfs -q -L ROOT /dev/mapper/root
+
+
+#Making subvols
 mount /dev/mapper/root /mnt
-btrfs su cr /mnt/@          #General Root
-btrfs su cr /mnt/@etc       #Root system config, and defaults
-btrfs su cr /mnt/@var       #Mail
-btrfs su cr /mnt/@var_log   #Logs
-btrfs su cr /mnt/@opt       #Matlab
-btrfs su cr /mnt/@snap      #Backups, snapshots
-btrfs su cr /mnt/@usr       #less compression, all binaries.
-btrfs su cr /mnt/@srv       #Personal Website
-    #User sub system
-btrfs su cr /mnt/@taha          #General stuff in home folder, doenst get backed up with snaps
-btrfs su cr /mnt/@taha_config   #All program config.
-btrfs su cr /mnt/@taha_devel    #I want this backed up often
-btrfs su cr /mnt/@taha_down     #Downloads folder, no datacow
-btrfs su cr /mnt/@taha_media    #Media-> Movies, books, pictures, music
-btrfs su cr /mnt/@taha_local    #System config-> GTK theme, icons, fonts, custom programs(neofetch)
-#btrfs su cr /mnt/@taha_bin      #Custom neofetch, this is inside of .local
-btrfs su cr /mnt/@taha_research #My research -> experiments, papers, other work.
-btrfs su cr /mnt/@taha_work     #Work files (manufactoring, tutoring, pay stubs)
-btrfs su cr /mnt/@taha_school   #Files related to school, past labs, etc
-btrfs su cr /mnt/@taha_person   #Bank files, resumes, copies of important files related to me.
-#Person should be encrypted to protect from bad programs.
+    #ROOT
+btrfs su cr /mnt/@beryllium #General Root, exec
+btrfs su cr /mnt/@snap      #Backups, snapshots, no exec
+    #USER
+btrfs su cr /mnt/@taha          #no execution
+btrfs su cr /mnt/@taha_devel    #Allow execution
+btrfs su cr /mnt/@taha_sys      #Allow execution (system config stuff)
 umount /mnt
 
 
-#Runtime are light compressed (bin, usr)        level 1
-#change often are moderate compressed. (var)    level 3
-#low change are heavy compressed (etc, config)  level 6
-#noatime -> reduce writes
-#ssd -> ssd optimization
-#nodiscard -> remove trim.
-#compress -> compression
-mount -o noatime,ssd,nodiscard,compress=zstd:3,subvol=@ /dev/mapper/sys /mnt
-    #Top level folders
-mkdir /mnt/{boot,etc,home,opt,srv,usr,var,.snap}
-#mount -o noatime,ssd,nodiscard,compress=zstd:6,subvol=@etc /dev/mapper/sys /mnt/etc
-mount -o noatime,ssd,nodiscard,compress=zstd:6,subvol=@opt /dev/mapper/sys /mnt/opt
-mount -o noatime,ssd,nodiscard,compress=zstd:6,subvol=@srv /dev/mapper/sys /mnt/srv
-#mount -o noatime,ssd,nodiscard,compress=zstd:1,subvol=@usr /dev/mapper/sys /mnt/usr
-mount -o noatime,ssd,nodiscard,compress=zstd:3,subvol=@var /dev/mapper/sys /mnt/var
-mount -o noatime,ssd,nodiscard,compress=zstd:6,subvol=@snap /dev/mapper/sys /mnt/.snap
-    #Sub level 1.
-mkdir /mnt/var/log
+# Mounting
+# Beryllium, Gallium
+mount -o noatime,ssd,nodiscard,compress=zstd:3,subvol=@beryllium /dev/mapper/root /mnt
+    # ROOT
+mkdir /mnt/{boot,media,.snap,home}
+mount -o nodev,nosuid,noexec $bootdrive /mnt/boot
+mount -o noatime,ssd,nodiscard,compress=zstd:3,subvol=@opt /dev/mapper/root /mnt/opt
+mount -o noatime,ssd,nodiscard,compress=zstd:3,subvol=@snap /dev/mapper/root /mnt/.snap
+    # USER
 mkdir /mnt/home/taha
-mount -o noatime,ssd,nodiscard,compress=zstd:3,subvol=@var_log /dev/mapper/sys /mnt/var/log
-mount -o noatime,ssd,nodiscard,compress=zstd:3,subvol=@taha /dev/mapper/sys /mnt/home/taha
-    #Sub level 2
-mkdir /mnt/home/taha/{.config,.local,devel,down,media,research,work,school,person}
-mount -o noatime,ssd,nodiscard,compress=zstd:6,subvol=@taha_config /dev/mapper/sys /mnt/home/taha/.config
-mount -o noatime,ssd,nodiscard,compress=zstd:1,subvol=@taha_local /dev/mapper/sys /mnt/home/taha/.local
-mount -o noatime,ssd,nodiscard,compress=zstd:3,subvol=@taha_devel /dev/mapper/sys /mnt/home/taha/devel
-mount -o noatime,ssd,nodiscard,nodatadow,compress=zstd:3,subvol=@taha_down /dev/mapper/sys /mnt/home/taha/down
-mount -o noatime,ssd,nodiscard,compress=zstd:3,subvol=@taha_media /dev/mapper/sys /mnt/home/taha/media
-mount -o noatime,ssd,nodiscard,compress=zstd:6,subvol=@taha_research /dev/mapper/sys /mnt/home/taha/research
-mount -o noatime,ssd,nodiscard,compress=zstd:6,subvol=@taha_work /dev/mapper/sys /mnt/home/taha/work
-mount -o noatime,ssd,nodiscard,compress=zstd:6,subvol=@taha_school /dev/mapper/sys /mnt/home/taha/school
-mount -o noatime,ssd,nodiscard,compress=zstd:6,subvol=@taha_person /dev/mapper/sys /mnt/home/taha/person
-    #Boot mounting
-mount $bootdrive /mnt/boot
-    #Swap file
+mkdir /mnt/home/taha/{devel,sys,.cache,.tmp}
+mount -o noatime,ssd,nodiscard,nosuid,noexec,nodev,compress=zstd:3,subvol=@taha /dev/mapper/root /mnt/home/taha
+mount -o noatime,ssd,nodiscard,compress=zstd:1,subvol=@taha_devel /dev/mapper/root /mnt/home/taha/devel
+mount -o noatime,ssd,nodiscard,compress=zstd:3,subvol=@taha_sys /dev/mapper/root /mnt/home/taha/sys
+    # SWAP
 truncate -s 0 /mnt/.swapfile
 chattr +C /mnt/.swapfile
 btrfs property set /mnt/.swapfile compression none
@@ -119,12 +88,12 @@ mkswap /mnt/.swapfile
 swapon /mnt/.swapfile
 
 
-#Making ghe base system
+#Base packages and FSTAB
 pacstrap /mnt base linux-hardened linux-firmware intel-ucode zsh doas nano nvim ranger
 genfstab -U /mnt >> /mnt/etc/fstab
 echo "tmpfs	/tmp	tmpfs	rw,nosuid,noatime,nodev	0 0" >> /mnt/etc/fstab
-echo "tmpfs	/home/taha/tmp	tmpfs	rw,nosuid,noatime,nodev	0 0" >> /mnt/etc/fstab
 echo "tmpfs	/home/taha/.cache	tmpfs	rw,nosuid,noatime,nodev	0 0" >> /mnt/etc/fstab
+echo "tmpfs	/home/taha/.tmp	tmpfs	rw,nosuid,noatime,nodev	0 0" >> /mnt/etc/fstab 
 
 #Pacman Optimizations
 sed -i 's/#ParallelDownloads/ParallelDownloads/g' /mnt/etc/pacman.conf      #Pacman packages
